c3f3bdd0f82dc93f0f923fc90979b69794438eac
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-30 16:05:08 +0000

    DockingStation responds to release_bike

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,11 @@
 require 'boris_bikes'
 
 describe DockingStation do
-  #docking_station = DockingStation.new
+  it 'responds to message to release_bike' do
+    docking_station = DockingStation.new
+
+    expect(docking_station).to respond_to(:release_bike)
+  end
+
+
 end
