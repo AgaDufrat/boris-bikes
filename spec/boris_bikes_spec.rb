4feb2e555c1087bc19165f4727b1618ac4099d66
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-30 16:10:09 +0000

    refactored rspec to use one-liner syntax

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,11 +1,5 @@
 require 'boris_bikes'
 
 describe DockingStation do
-  it 'responds to message to release_bike' do
-    docking_station = DockingStation.new
-
-    expect(docking_station).to respond_to(:release_bike)
-  end
-
-
+  it { should respond_to(:release_bike) }
 end
